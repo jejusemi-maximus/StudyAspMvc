f727c7f35512184f8d111eb527ee430eb96bed70
Maxmimus.kim <maximus.kim@jeju-semi.com>
2021-03-12 14:53:23 +0900

    form연결 및 람다식 활용

---
+++ StudyFisrtMVC/StudyFisrtMVC/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace StudyFisrtMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ViewResult Index()
+        {
+            ViewBag.time = DateTime.Now.Day;
+            return View();
+        }
+        
+        public ViewResult Inviteform()
+        {
+            return View();
+        }
+    }
+}
---
+++ StudyFisrtMVC/StudyFisrtMVC/Models/Guests.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace StudyFisrtMVC.Models
+{
+    public class Guests
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+
+        public string Phone { get; set; }
+        public bool? Attend { get; set; }
+    }
+}
