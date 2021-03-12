850785fb53983b51a8e16d54fadcf02f6b763ce9
Maxmimus.kim <maximus.kim@jeju-semi.com>
2021-03-12 18:32:38 +0900

    3.12 end

---
+++ StudyFisrtMVC/StudyFisrtMVC/Controllers/TestController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace StudyFisrtMVC.Controllers
+{
+    public class TestController : Controller
+    {
+        private IUserRepo repo;
+        // GET: Admin
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
