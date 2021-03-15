b1efd20e9cd76b8f93104474bc78e32f44341827
Maxmimus.kim <maximus.kim@jeju-semi.com>
2021-03-15 14:33:46 +0900

    commit

--- LanguageFeatures/LanguageFeatures/Controllers/HomeController.cs
+++ LanguageFeatures/LanguageFeatures/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using LanguageFeatures.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -12,6 +13,20 @@ namespace LanguageFeatures.Controllers
         public string Index()
         {
             return "es,es,e,se";
+        }
+
+        public ActionResult AutoProperty()
+        {
+            Products products = new Products();
+            products.Name = "it";
+            string Pname = products.Name;
+            return View("Result",(object)Pname);
+        }
+
+        public ViewResult CreateProduct()
+        {
+            Products product = new Products();
+
         }
     }
 }
---
+++ LanguageFeatures/LanguageFeatures/Models/Products.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+using System.Web;
+
+namespace LanguageFeatures.Models
+{
+    public class Products
+    {
+        public string Name { get; set; }
+
+        public int Price { get; set; }
+        
+        var msme = Products.Sum()
+        
+        
+    }
+}
