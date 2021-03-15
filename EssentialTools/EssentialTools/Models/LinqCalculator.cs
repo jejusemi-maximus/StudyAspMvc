35eb60063d1a995f40d45ad200c34c99dfa364d8
Maxmimus.kim <maximus.kim@jeju-semi.com>
2021-03-15 14:47:00 +0900

    프로젝트 생성 및 Linq사용

---
+++ EssentialTools/EssentialTools/Models/LinqCalculator.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EssentialTools.Models
+{
+    public class LinqCalculator
+    {
+        public decimal SumProduct(IEnumerable<Product> prod)
+        {
+            return prod.Sum(p => p.Price);
+        }
+    }
+}
