4ac21cfd8c9f7e78d9d6ccc31fadaea09d30722a
Maxmimus.kim <maximus.kim@jeju-semi.com>
2021-03-17 15:35:10 +0900

    페이지 분할 메서드 생성 및 TDD를 통한 검증

--- SportStore/SportStore.WebUI/Controllers/ProductController.cs
+++ SportStore/SportStore.WebUI/Controllers/ProductController.cs
@@ -1,8 +1,6 @@
 ﻿using SportStore.Domain.Repository;
-using System;
-using System.Collections.Generic;
+using SportStore.WebUI.Models;
 using System.Linq;
-using System.Web;
 using System.Web.Mvc;
 
 namespace SportStore.WebUI.Controllers
@@ -10,15 +8,32 @@ namespace SportStore.WebUI.Controllers
     public class ProductController : Controller
     {
         private IProductRepository repository;
+        public int PageSize = 4;
 
         public ProductController(IProductRepository productRepo)
         {
-            repository = productRepo;
+            this.repository = productRepo;
         }
         // GET: Product
-        public ActionResult List()
+        public ViewResult List(int page = 1)
         {
-            return View(repository.Products);
+            ProductListViewModel model = new ProductListViewModel
+            {
+                Products = repository.Products
+                    .OrderBy(p => p.ProductID)
+                    .Skip((page - 1) * PageSize)
+                    .Take(PageSize),
+
+                PagingInfo = new PagingInfo
+                {
+                    CurrentPage = page,
+                    ItemsPerPage = PageSize,
+                    TotalItems = repository.Products.Count()
+                }
+            };
+            return View(model);
         }
+
     }
+
 }
