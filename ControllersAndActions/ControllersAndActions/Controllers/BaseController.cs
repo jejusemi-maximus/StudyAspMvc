b01351b575e9abee2c7112dd5af63d70efeb250d
Maxmimus.kim <maximus.kim@jeju-semi.com>
2021-03-24 18:09:43 +0900

    시작

---
+++ ControllersAndActions/ControllersAndActions/Controllers/BaseController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace ControllersAndActions.Controllers
+{
+    public class BaseController : IController
+    {
+        public void Execute(RequestContext requestContext)
+        {
+            string controller = (string)requestContext.RouteData.Values["controller"];
+            string action = (string)requestContext.RouteData.Values["action"];
+
+            requestContext.HttpContext.Response.Write(string.Format("컨트롤러 {0} , 액션 {1}", controller, action));
+
+        }
+    }
+}
