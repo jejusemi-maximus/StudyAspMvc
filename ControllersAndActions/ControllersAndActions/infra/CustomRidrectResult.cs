28a16d2181318b9e93ef72e3fd9c300a1c3528f9
chimaek <pipiru100@gmail.com>
2021-03-25 15:54:29 +0900

    마지막 추가

---
+++ ControllersAndActions/ControllersAndActions/infra/CustomRidrectResult.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ControllersAndActions.infra
+{
+    public class CustomRidrectResult : ActionResult
+    {
+        public string url { get; set; }
+
+        public override void ExecuteResult(ControllerContext context)
+        {
+            string fullUrl = UrlHelper.GenerateContentUrl(url, context.HttpContext);
+            context.HttpContext.Response.Redirect(fullUrl);
+        }
+    }
+}
